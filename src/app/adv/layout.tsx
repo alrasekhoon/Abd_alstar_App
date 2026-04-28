b07a60de6468c3f17f7ddde044a6e3eb4027ccac
alrasekhoon <alrasekhooninlaw.app@gmail.com>
2026-04-28 17:32:18 +0300

    Update layout.tsx

--- src/app/adv/layout.tsx
+++ src/app/adv/layout.tsx
@@ -1,31 +1,3 @@
-'use client'
-
-import Sidebar from '../components/Sidebar'
-import TopNavbar from '../components/TopNavbar'
-
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <div className="flex flex-col h-screen overflow-hidden bg-gray-50 text-right" dir="rtl">
-      
-      {/* القائمة العلوية */}
-      <TopNavbar />
-
-      <div className="flex flex-1 overflow-hidden">
-        
-        {/* القائمة الجانبية */}
-        <Sidebar />
-
-        {/* محتوى الصفحة */}
-        <main className="flex-1 overflow-y-auto p-4 md:p-8">
-          {children}
-        </main>
-
-      </div>
-    </div>
-  )
+export default function AdvLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>
 }
-
