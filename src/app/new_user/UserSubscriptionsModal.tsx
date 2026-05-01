915827aa8e6e440987236831fa0a154a55d7153f
alrasekhoon <alrasekhooninlaw.app@gmail.com>
2026-04-30 18:59:10 -0700

    Update UserSubscriptionsModal.tsx

--- src/app/new_user/UserSubscriptionsModal.tsx
+++ src/app/new_user/UserSubscriptionsModal.tsx
@@ -208,8 +208,8 @@ export default function UserSubscriptionsModal({
 
 setQuickNotification({
   isOpen: true,
-  title: 'اشتراك جديد',
-  body: `عزيزي الطالب،\nتمت إضافة "${savedMaterialName}" إلى سلة مشترياتك بنجاح.\n• نوع الاشتراك: ${savedType}.\n• نوع الخدمة: ${savedType}.\n• القيمة: ${Number(newSubscription.price1).toLocaleString()} ل.س.\nمع خالص أمنياتنا لكم بالتفوق والنجاح.`
+  title: 'إضافة اشتراك جديد',
+  body: `عزيزي الطالب،\nتمت إضافة "${savedMaterialName}" إلى سلة مشترياتك بنجاح.\n• نوع الاشتراك: ${savedType}.\n• نوع الخدمة: \n• القيمة: \nمع خالص أمنياتنا لكم بالتفوق والنجاح.`
 });
 
     } catch (err) {
