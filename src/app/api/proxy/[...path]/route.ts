c0ad09b3595ab036d93fd188d312e39dad57636f
alrasekhoon <alrasekhooninlaw.app@gmail.com>
2026-05-01 05:00:04 -0700

    Update route.ts

--- src/app/api/proxy/[...path]/route.ts
+++ src/app/api/proxy/[...path]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-const API_BASE_URL = 'https://alrasekhooninlaw.com/alraskoon/v2';
+const API_BASE_URL = process.env.CUSTOM_API_URL;//|| 'http://alraskun.atwebpages.com';
 
 // رؤوس افتراضية لمنع التخزين المؤقت للردود JSON
 const NO_CACHE_JSON_HEADERS = {
