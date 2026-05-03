1bcae0abdfd177a7e1f693ba02be18b5dfdea62d
alrasekhoon <alrasekhooninlaw.app@gmail.com>
2026-05-03 08:56:57 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import './globals.css'
-import { cookies } from 'next/headers'
 import Sidebar from './components/Sidebar'
 import Header from './components/Header'
 
@@ -8,22 +7,15 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const cookieStore = cookies()
-  const isLoggedIn = !!cookieStore.get('authToken')?.value
-
   return (
     <html lang="ar" dir="rtl">
       <body>
-        {isLoggedIn ? (
-          <Sidebar>
-            <Header />
-            <main className="flex-1 overflow-y-auto p-6 bg-gray-50">
-              {children}
-            </main>
-          </Sidebar>
-        ) : (
-          <main>{children}</main>
-        )}
+        <Sidebar>
+          <Header />
+          <main className="flex-1 overflow-y-auto p-6 bg-gray-50">
+            {children}
+          </main>
+        </Sidebar>
       </body>
     </html>
   )
