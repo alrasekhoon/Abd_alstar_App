01e75450b2b42f7731464a53a6f777b75bdcffc0
alrasekhoon <alrasekhooninlaw.app@gmail.com>
2026-05-04 02:55:02 -0700

    Update UserTransactionsModal.tsx

--- src/app/new_user/UserTransactionsModal.tsx
+++ src/app/new_user/UserTransactionsModal.tsx
@@ -341,7 +341,7 @@ export default function UserTransactionsModal({
       <div className="bg-white rounded-xl shadow-2xl w-full max-w-6xl h-[95vh] md:h-auto md:max-h-[90vh] overflow-hidden relative z-10 flex flex-col">
         
         {/* الهيدر */}
-        <div className="flex justify-between items-center p-4 md:p-6 border-b border-gray-200 bg-gray-50 flex-wrap gap-4 shrink-0">
+        <div className="flex flex-row-reverse justify-between items-center p-4 md:p-6 border-b border-gray-200 bg-gray-50 flex-wrap gap-4 shrink-0">
           <div>
             <h2 className="text-xl md:text-2xl font-extrabold text-gray-900 tracking-tight">إدارة الدفعات المالية</h2>
             <p className="text-xs md:text-sm text-gray-600 mt-1 font-medium">
