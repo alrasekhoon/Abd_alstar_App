75d3043988ad595560eaf2de9165a4af4df12db4
alrasekhoon <alrasekhooninlaw.app@gmail.com>
2026-05-05 08:06:06 -0700

    Update page.tsx

--- src/app/mony1/page.tsx
+++ src/app/mony1/page.tsx
@@ -464,6 +464,7 @@ export default function MoneyManagement() {
                           )}
                         </div>
                       </div>
+                    </td>
                   </tr>
                 );
               })}
