b9c351fecb753426e57729962ef46be2b9f6bd5c
alrasekhoon <alrasekhooninlaw.app@gmail.com>
2026-05-08 12:22:13 -0700

    Update page.tsx

--- src/app/uni_material/page.tsx
+++ src/app/uni_material/page.tsx
@@ -409,7 +409,7 @@ export default function UniMaterialPage() {
                 </div>
               </div>
 
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div>
                   <label className="block text-sm font-bold text-gray-700 mb-1.5">الفصل *</label>
                   <select
