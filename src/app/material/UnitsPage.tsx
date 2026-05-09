4c6b39166c3f095f5d5f428e31da5eb9e6f3f048
alrasekhoon <alrasekhooninlaw.app@gmail.com>
2026-05-08 17:48:10 -0700

    Update UnitsPage.tsx

--- src/app/material/UnitsPage.tsx
+++ src/app/material/UnitsPage.tsx
@@ -1076,7 +1076,7 @@ const handleSaveBulkRows = async () => {
                             >
                               <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M9 2a2 2 0 00-2 2v8a2 2 0 002 2h6a2 2 0 002-2V6.414A2 2 0 0016.414 5L14 2.586A2 2 0 0012.586 2H9z" />
-                                <path d="M3 8a2 2 0 012-2v10h8a2 2 0 01-2 2H5a2 2 0 01-2-2V8z" />
+                           <path d="M3 8a2 2 0 012-2v10h8a2 2 0 01-2 2H5a2 2 0 01-2-2V8z" />
                               </svg>
                               الأسئلة
                             </button>
@@ -1089,13 +1089,9 @@ const handleSaveBulkRows = async () => {
               </tbody>
             </table>
           </div>
-        )}
-
-</tbody>
-            </table>
-          </div>
 
           {/* Bulk add controls */}
+
           <div className="flex items-center gap-3 mt-3">
             <button
               onClick={handleAddBulkRow}
